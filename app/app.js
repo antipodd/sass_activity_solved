2e4a50f928916a1099f7d57111181f423df4d4c8
Randy Burgess <wrburgess@gmail.com>
2017-06-28 17:14:24 -0500

    edits

--- app/app.js
+++ app/app.js
@@ -1,18 +1,20 @@
-// Include the Main React Dependenciesvar React = require("react");
-var React = require("react");
-var ReactDOM = require("react-dom");
+// Include the Main React Dependenciesvar React = require('react');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require('./public/main.scss')
 
 // Include the Header Component
-var Header = require("./components/Header");
-var Body = require("./components/Body");
-var Footer = require("./components/Footer");
+var Header = require('./components/Header');
+var Body = require('./components/Body');
+var Footer = require('./components/Footer');
 
 ReactDOM.render(
-  <div class="page-wrapper">
+  <div class='page-wrapper'>
     <Header />
     <Body />
     <Footer />
   </div>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
 
